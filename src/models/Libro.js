346614369ba178f3036de7c79f5f8749e6c081dd
JuanDiego <solanocorreajuandiego@gmail.com>
2026-07-17 14:31:11 -0500

    Add _id field to Libro schema

--- src/models/Libro.js
+++ src/models/Libro.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const LibroSchema = new mongoose.Schema({
-
+  _id: {
+    type: String,
+    required: true
+  },
   titulo: {
     type: String,
     required: true
